f04d4a159bf79cab074acf365cc23d42965a7ecf
Zzcc <1798231822@qq.com>
2022-01-29 11:29:06 +0800

    修改端口号

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -86,7 +86,7 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       path: '',
-      port: 3000,
+      port: 888,
       hostname: '0.0.0.0',
     }
   };
--- server/vite-react-server.js
+++
@@ -1,6 +0,0 @@
-const server = require('pushstate-server')
-
-server.start({
-  port: 8848,
-  directory: './app/h5'
-})
